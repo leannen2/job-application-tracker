0dfbf47397da041594986c1d49e90767234145dd
Leanne <leannen2@uci.edu>
2024-08-11 00:58:15 -0700

    start on backend

---
+++ job-application-tracker-backend/src/main/java/com/example/job_application_tracker/controller/JobController.java
@@ -0,0 +1,30 @@
+package com.example.job_application_tracker.controller;
+
+import com.example.job_application_tracker.dto.AddJobRequest;
+import com.example.job_application_tracker.service.JobService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class JobController {
+
+    private final JobService jobService;
+
+    @GetMapping("/jobs")
+    public Mono<List<String>> jobs() {
+        return Mono.just(jobService.jobs());
+    }
+
+    @PostMapping("/add-job")
+    public Mono<Void> addJob(@RequestBody AddJobRequest job) {
+//        jobService.addJob(job);
+        return Mono.empty();
+    }
+}
