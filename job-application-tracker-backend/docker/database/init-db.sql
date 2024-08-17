7f2154e6d0bdc69f57422282447fecdfa12ed5e5
nguyenliliana <liliananguyen@ucsb.edu>
2024-08-17 16:33:26 -0700

    Set up table

--- job-application-tracker-backend/docker/database/init-db.sql
+++ job-application-tracker-backend/docker/database/init-db.sql
@@ -6,7 +6,10 @@ CREATE TABLE IF NOT EXISTS jobs (
     id INT AUTO_INCREMENT PRIMARY KEY,
     company VARCHAR(255) NOT NULL,
     role VARCHAR(255) NOT NULL,
-    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    link VARCHAR(255),
+    stage ENUM('APPLIED', 'OA', 'RECRUITER_CALL', '1ST_ROUND', '2ND_ROUND', 'NEXT_ROUND', 'FINAL_ROUND', 'OFFER', 'REJECTED') NOT NULL DEFAULT 'APPLIED'
+    );
 
-INSERT INTO jobs (company, role) VALUES ('Microsoft', 'SWE');
+-- INSERT INTO jobs (company, role, stage) VALUES ('Microsoft', 'SWE', 'APPLIED');
+-- INSERT INTO jobs (company, role, link, stage) VALUES ('Microsoft', 'SWE', 'link.com', 'APPLIED');
