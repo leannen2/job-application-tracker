02f357101d8111eb086c4093d01249b3fe5f15ce
nguyenliliana <liliananguyen@ucsb.edu>
2024-08-17 16:48:04 -0700

    Change TIMESTAMP to DATE, add oa_deadline col, and sep OA stages

--- job-application-tracker-backend/docker/database/init-db.sql
+++ job-application-tracker-backend/docker/database/init-db.sql
@@ -6,10 +6,11 @@ CREATE TABLE IF NOT EXISTS jobs (
     id INT AUTO_INCREMENT PRIMARY KEY,
     company VARCHAR(255) NOT NULL,
     role VARCHAR(255) NOT NULL,
-    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    submitted_at DATE DEFAULT CURRENT_DATE,
+    oa_deadline DATE,
     link VARCHAR(255),
     stage ENUM('APPLIED', 'OA', 'RECRUITER_CALL', '1ST_ROUND', '2ND_ROUND', 'NEXT_ROUND', 'FINAL_ROUND', 'OFFER', 'REJECTED') NOT NULL DEFAULT 'APPLIED'
-    );
+);
 
 -- INSERT INTO jobs (company, role, stage) VALUES ('Microsoft', 'SWE', 'APPLIED');
 -- INSERT INTO jobs (company, role, link, stage) VALUES ('Microsoft', 'SWE', 'link.com', 'APPLIED');
