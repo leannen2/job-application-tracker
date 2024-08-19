fe904a21a6ef05258039b3710e274228752839e9
Leanne <leannen2@uci.edu>
2024-08-18 19:48:50 -0700

    get jobs works

--- job-application-tracker-backend/docker/database/init-db.sql
+++ job-application-tracker-backend/docker/database/init-db.sql
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS jobs (
     id INT AUTO_INCREMENT PRIMARY KEY,
     company VARCHAR(255) NOT NULL,
     role VARCHAR(255) NOT NULL,
-    submitted_at DATE DEFAULT CURRENT_DATE,
-    oa_deadline DATE,
+    submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    oa_deadline TIMESTAMP,
     link VARCHAR(255),
     stage ENUM('APP_NOT_SUBMITTED', 'APP_SUBMITTED', 'OA_RECEIVED', 'OA_COMPLETED', 'RECRUITER_CALL', '1ST_ROUND', '2ND_ROUND', 'NEXT_ROUND', 'FINAL_ROUND', 'OFFER', 'REJECTED') NOT NULL DEFAULT 'APP_NOT_SUBMITTED'
 );
 
--- INSERT INTO jobs (company, role, stage) VALUES ('Microsoft', 'SWE', 'APPLIED');
--- INSERT INTO jobs (company, role, link, stage) VALUES ('Microsoft', 'SWE', 'link.com', 'APPLIED');
+INSERT INTO jobs (company, role, stage) VALUES ('Microsoft', 'SWE', 'APP_SUBMITTED');
+INSERT INTO jobs (company, role, link, stage) VALUES ('Microsoft', 'SWE', 'link.com', 'APP_SUBMITTED');
