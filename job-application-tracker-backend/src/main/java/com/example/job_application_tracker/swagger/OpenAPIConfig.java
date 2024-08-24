8ff49d7867c83ec19e3a4cba8f143cf329c73ff3
nguyenliliana <liliananguyen@ucsb.edu>
2024-08-24 16:57:03 -0700

    Added dependencies and created OpenAPIConfig

---
+++ job-application-tracker-backend/src/main/java/com/example/job_application_tracker/swagger/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package com.example.job_application_tracker.swagger;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI baseOpenAPI(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Job-Tracker Spring Project OpenAPI Docs")
+                        .version("1.0.0").description("Documentation description"));
+    }
+}
