d2064a9a71eac2e559b5db39cfdb2f076eacdeab
leannen2 <leannen2@uci.edu>
2024-11-29 14:02:19 +0900

    start on Job component

--- job-application-tracker-ui/src/App.tsx
+++ job-application-tracker-ui/src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css";
+import Job from "./components/Job"
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <div className="App">Hello World
+  <Job/>
+  </div>;
 }
 
 export default App;
