78ac8a6d1195156854e22bd223fdc43f5d152bc3
leannen2 <leannen2@uci.edu>
2024-12-01 19:07:14 +0900

    added cross origins

--- job-application-tracker-backend/src/main/java/com/example/job_application_tracker/controller/JobController.java
+++ job-application-tracker-backend/src/main/java/com/example/job_application_tracker/controller/JobController.java
@@ -3,16 +3,14 @@ package com.example.job_application_tracker.controller;
 import com.example.job_application_tracker.repository.Job;
 import com.example.job_application_tracker.service.JobService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class JobController {
 
     private final JobService jobService;
