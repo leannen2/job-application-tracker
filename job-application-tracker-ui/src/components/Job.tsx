034186f5fc3db934ac8b84b023ce1dc9934f7560
leannen2 <leannen2@uci.edu>
2024-12-01 18:33:55 +0900

    Displays jobs in a table

--- job-application-tracker-ui/src/components/Job.tsx
+++ job-application-tracker-ui/src/components/Job.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { JobType } from "../types";
 
-function Job() {
-    return (
-    <div>
-        Job
-    </div>);
+function Job(job: JobType) {
+  return (
+    <tr>
+      <td>{job.id}</td>
+      <td>{job.company}</td>
+      <td>{job.role}</td>
+      <td>{job.submittedAt}</td>
+      <td>{job.oaDeadline}</td>
+      <td>
+        {job.link != null && (
+          <a
+            href={job.link != null ? job.link : undefined}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Link
+          </a>
+        )}
+      </td>
+      <td>{job.stage}</td>
+    </tr>
+  );
 }
 
 export default Job;
