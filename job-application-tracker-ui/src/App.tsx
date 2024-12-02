545275c2c31acbac78ecc48f663953fe227ff19e
leannen2 <leannen2@uci.edu>
2024-12-02 22:28:28 +0900

    fix sort bug and add icon to header to indicate which column is sorted

--- job-application-tracker-ui/src/App.tsx
+++ job-application-tracker-ui/src/App.tsx
@@ -4,6 +4,8 @@ import jobList from "./data/jobs-list-example.json";
 import { fetchJobs } from "./api/jobApi";
 import { useState, useEffect } from "react";
 import { JobType } from "./types";
+import { ReactComponent as ArrowUp } from "./assets/arrow-up.svg";
+import { ReactComponent as ArrowDown } from "./assets/arrow-down.svg";
 
 function App() {
   const [jobs, setJobs] = useState<Array<JobType>>([]);
@@ -25,9 +27,9 @@ function App() {
       });
   }, []);
 
+  // sort jobs when sortConfig changes
   useEffect(() => {
     const sortedJobs = [...jobs];
-    console.log(sortConfig);
     if (sortConfig) {
       sortedJobs.sort((a, b) => {
         const key = sortConfig.key;
@@ -48,17 +50,21 @@ function App() {
           if (valueA > valueB) {
             return direction === "ascending" ? 1 : -1;
           }
+        } else {
+          if (valueA === undefined || valueA === null) {
+            return direction === "ascending" ? -1 : 1;
+          } else {
+            return direction === "ascending" ? 1 : -1;
+          }
         }
 
         return 0;
       });
     }
     setJobs([...sortedJobs]);
-    console.log("jobs", jobs);
   }, [sortConfig]);
 
   const handleSort = (key: keyof JobType) => {
-    console.log("handle sort");
     let direction = "ascending";
     if (
       sortConfig &&
@@ -70,27 +76,62 @@ function App() {
     setSortConfig({ key, direction });
   };
 
+  const getSortIcon = (key: keyof JobType) => {
+    if (sortConfig.key === key) {
+      return sortConfig.direction === "ascending" ? (
+        <ArrowUp className="sort-icon" />
+      ) : (
+        <ArrowDown className="sort-icon" />
+      );
+    }
+    return <ArrowUp className="sort-icon" style={{ visibility: "hidden" }} />;
+  };
+
   return (
     <div className="App">
       <h1 id="tracker-list-header">Job Application Tracker</h1>
-      <table border={1}>
-        <thead>
-          <tr>
-            <th onClick={() => handleSort("id")}>ID</th>
-            <th onClick={() => handleSort("company")}>Company</th>
-            <th onClick={() => handleSort("role")}>Role</th>
-            <th onClick={() => handleSort("submittedAt")}>Submitted At</th>
-            <th onClick={() => handleSort("oaDeadline")}>OA Deadline</th>
-            <th onClick={() => handleSort("link")}>Link</th>
-            <th onClick={() => handleSort("stage")}>Stage</th>
-          </tr>
-        </thead>
-        <tbody>
-          {jobs.map((job) => (
-            <Job {...job} key={job.id} />
-          ))}
-        </tbody>
-      </table>
+      <div id="table-container">
+        <table border={1}>
+          <thead>
+            <tr>
+              <th onClick={() => handleSort("id")}>
+                ID
+                {getSortIcon("id")}
+              </th>
+              <th onClick={() => handleSort("company")}>
+                Company
+                {getSortIcon("company")}
+              </th>
+              <th onClick={() => handleSort("role")}>
+                Role
+                {getSortIcon("role")}
+              </th>
+              <th onClick={() => handleSort("submittedAt")}>
+                Submitted At
+                {getSortIcon("submittedAt")}
+              </th>
+              <th onClick={() => handleSort("oaDeadline")}>
+                OA Deadline
+                {getSortIcon("oaDeadline")}
+              </th>
+              <th onClick={() => handleSort("link")}>
+                Link
+                {getSortIcon("link")}
+              </th>
+              <th onClick={() => handleSort("stage")}>
+                Stage
+                {getSortIcon("stage")}
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {jobs.map((job) => (
+              <Job {...job} key={job.id} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+
       {error !== "" && (
         <p id="error-text">
           Backend is not currently running, so example data has been loaded.
