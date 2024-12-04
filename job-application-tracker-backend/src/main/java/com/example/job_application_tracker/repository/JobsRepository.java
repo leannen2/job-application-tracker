3aba69350e04285f9ac4846ccf59bc6cf636fcf9
leannen2 <leannen2@uci.edu>
2024-12-04 13:20:07 +0900

    Updates

--- job-application-tracker-backend/src/main/java/com/example/job_application_tracker/repository/JobsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.job_application_tracker.repository;
-
-import org.springframework.data.r2dbc.repository.R2dbcRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobsRepository extends R2dbcRepository<Job, Integer> {
-}
--- job-application-tracker-ui/src/types.ts
+++
@@ -1,9 +0,0 @@
-export interface JobType {
-  id: number;
-  company: string;
-  role: string;
-  submittedAt: String | null;
-  oaDeadline: String | null;
-  link: string | null;
-  stage: string;
-}
